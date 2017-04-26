c7cd2a697f0ad2c8b43f855103ad4ce7f9ad0542
marce <marce@MV-WIN10DEV>
2017-04-26 08:16:26 -0300

    Crear un proyecto de ejemplos de Colas

---
+++ Chapter10Queues/Chapter10Queues/Pasajero.cs
@@ -0,0 +1,16 @@
+﻿namespace Chapter10Queues
+{
+    class Pasajero
+    {
+        public string Nombre { get; private set; }
+        public int Fila { get; private set; }
+        public string Asiento { get; private set; }
+
+        public Pasajero(string nombre, int fila, string asiento)
+        {
+            Nombre = nombre;
+            Fila = fila;
+            Asiento = asiento;
+        }
+    }
+}
---
+++ Chapter10Queues/Chapter10Queues/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chapter10Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EncabezadoYPieConsola separador = new EncabezadoYPieConsola();
+
+            #region Ejemplo 1: Cola simple
+            separador.EscribirEncabezado("Ejemplo 1: Cola simple");
+
+            Queue<int> cola = new Queue<int>();
+            cola.Enqueue(2);
+            cola.Enqueue(4);
+            cola.Enqueue(8);
+            Console.WriteLine("El primer elemento de la cola es: {0}", cola.Dequeue().ToString());
+
+            separador.EscribirPie("Fin Ejemplo 1");
+            #endregion
+
+            #region Ejemplo 2: Constructores
+            separador.EscribirEncabezado("Ejemplo 2: Constructores");
+
+            Queue<Pasajero> primeraClase = new Queue<Pasajero>(10);
+
+            primeraClase.Enqueue(new Pasajero("Charles Lindbergh", 3, "A"));
+            primeraClase.Enqueue(new Pasajero("Antoine de Saint Exupery", 1, "B"));
+            Pasajero elSiguiente = primeraClase.Dequeue();
+            Console.WriteLine("El primero en llegar fue: {0} que se sienta en {1}-{2}", elSiguiente.Nombre, elSiguiente.Fila.ToString(), elSiguiente.Asiento);
+
+            separador.EscribirPie("Fin de ejemplo 2");
+            #endregion
+
+
+        }
+    }
+
+    struct EncabezadoYPieConsola
+    {
+        public void EscribirEncabezado(string Titulo)
+        {
+            string encabezado = PrepararString(Titulo, '*');
+            Console.WriteLine(encabezado);
+            Console.WriteLine("");
+        }
+
+        public void EscribirPie(string Titulo)
+        {
+            string pie = PrepararString(Titulo, '-');
+            Console.WriteLine("");
+            Console.WriteLine(pie);
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+
+        private string PrepararString(string Literal, char Caracter)
+        {
+            if (Literal.Length > 80)
+                Literal = Literal.Substring(1, 80);
+            int numeroDeAsteriscos = 80 - Literal.Length;
+            string preparada = new string(Caracter, (int)(numeroDeAsteriscos / 2)) + Literal +
+            new string(Caracter, (int)(numeroDeAsteriscos / 2));
+            return preparada;
+        }
+    }
+
+}
