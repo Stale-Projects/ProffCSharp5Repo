e3b65c7d127bb51bfa13b84e5eec8c0b4e900cd1
Marcelo.Volta <mvolta@glasstone-mg.com>
2017-05-25 21:13:57 -0300

    Crear un ejemplo de ObservableCollection

---
+++ Capitulo10_ColeccionesObservables/Capitulo10_ColeccionesObservables/Notificador.cs
@@ -0,0 +1,65 @@
+﻿// ==++==
+// 
+//   Copyright (c) S. Marcelo Volta.  Todos los derechos reservados.
+// 
+// ==--==
+/*============================================================
+** Proyecto: Capitulo10_ColeccionesObservables
+** Struct:  MensajePersonalizado
+** 
+** <OWNER>MarceVolta</OWNER>
+**
+** Propósito: Proveer textos descriptivos para el manejador de eventos de la 
+** ObservableCollection usada en Main(). 
+** Es parte de los ejemplos de código para el capítulo 10 del libro
+** "De Cabeza a C#"
+** Descripciones debajo en el summary
+===========================================================*/
+
+
+using System.Collections.Specialized;
+
+namespace Capitulo10_ColeccionesObservables
+{
+    /// <summary>
+    /// Esta struct tiene un método estático que devuelve una string en base al valor del argumento
+    /// Este valor viene de la enumeración usada para llenar la propiedad Action de 
+    /// NotifyCollectionChangedAction
+    /// Observa que las string devueltas tienen tres partes, una para cada mensaje que se muestra
+    /// en la consola en respuesta al evento. 
+    /// En el Delegate que se usa para responder al evento en <see cref="Program"/> 
+    /// se separa la string usando una instrucción Split.
+    /// La idea de que el método sea estátitco es para evitar instanciar la Struct cada vez que se
+    /// dispara un evento. De este modo no es necesaria instanciarla.
+    /// </summary>
+    struct MensajePersonalizado
+    {
+
+        public static string Mensaje(NotifyCollectionChangedAction accion)
+        {
+            switch (accion)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    return "Se agregaron elementos, El primer elemento que se agregó en esta acción fue a la posición ,Se agregó el elemento ";
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    return "Se quitaron elementos, El primer elemento que se quitó en esta acción estaba en la posición ,Se quitó el elemento ";
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                    return "Se reemplazaron elementos, El primer elemento que se reemplazó en esta acción estaba en la posición ,Se agregó el elemento ";
+                    break;
+                case NotifyCollectionChangedAction.Move:
+                    return "Se movieron elementos, El primer elemento que se movió en esta acción estaba en la posición ,Se movió el elemento ";
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    return "Se reseteó la colección!";
+                    break;
+                default:
+                    return null;
+                    break;
+            }
+        }
+
+
+    }
+}
---
+++ Capitulo10_ColeccionesObservables/Capitulo10_ColeccionesObservables/Program.cs
@@ -0,0 +1,98 @@
+﻿// ==++==
+// 
+//   Copyright (c) S. Marcelo Volta.  Todos los derechos reservados.
+// 
+// ==--==
+/*============================================================
+** Proyecto: Capitulo10_ColeccionesObservables
+** Clase:  Program
+** 
+** <OWNER>MarceVolta</OWNER>
+**
+** Propósito: Proveer ejemplos de código para el capítulo 10 del libro
+** "De Cabeza a C#"
+** Este proyecto provee ejemplos de Colecciones Observables (ObservableCollection)
+* Ten en cuenta que debes referenciar el namespace System.Collections.ObjectModel
+* La funcionalidad está provista por el assembly WindowsBase
+* Además debemos referenciar System.Collections.Specialized para los 
+* argumentos del(los) Delegate(s) que maneja(n) el evento
+** Descripciones debajo en el summary
+===========================================================*/
+
+
+using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Capitulo10_ColeccionesObservables
+{
+    /// <summary>
+    /// Este es un ejemplo simple de Colecciones observables en el que simplemente creamos
+    /// la ObservableCollection y le asignamos un delegado a la propiedad CollectionChanged
+    /// En este delegado mostramos dos o tres mensajes que tienen que ver con el tipo de 
+    /// acción que desencadenó el evento. 
+    /// Para definir los mensajes usamos una Struct <see cref="MensajePersonalizado"/> 
+    /// de modo que el código de tratamiento de los eventos quede más limpio
+    /// En el manejador de evento recibimos dos objetos: el sender (de tipo object) 
+    /// y NotifyCollectionChangedEventArgs que contiene información sobre el evento: 
+    /// <list type="bullet">
+    /// <item>Action: es una enumeración del tipo de acción que originó el evento</item>
+    /// <item>OldItems es una lista que se llena con los elementos que fueron removidos
+    /// (si es que éste es el tipo de acción que disparó el evento)</item>
+    /// <item>OldStartingIndex es el índice del primer elemento que fue removido o reemplazado</item>
+    /// <item>NewItems es análogo a OldItems pero contiene los elementos que fueron agregados</item>
+    /// <item>NewStartingIndex es análogo a OldStartingIndex para el primer elemento agregado</item>
+    /// </list>
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numeros = new ObservableCollection<string>();
+            numeros.CollectionChanged += Numeros_Change;
+
+            numeros.Add("Uno");
+            numeros.Add("Tres");
+            numeros.Add("Cinco");
+            numeros.Add("Siete");
+            numeros.Add("Nueve");
+            numeros.Insert(1, "Dos");
+            numeros.RemoveAt(4);
+            numeros.Move(4, 2);
+            numeros[4] = "Reemplazado";
+        }
+
+        static void Numeros_Change(object sender, NotifyCollectionChangedEventArgs e)
+        {
+
+            string[] mensajes = MensajePersonalizado.Mensaje(e.Action).Split(',');
+            Console.WriteLine(mensajes[0]);
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                case NotifyCollectionChangedAction.Replace:
+                    Console.WriteLine(mensajes[1] + e.NewStartingIndex);
+                    foreach (var item in e.NewItems)
+                    {
+                        Console.WriteLine(mensajes[2] + item.ToString());
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                case NotifyCollectionChangedAction.Move:
+                    Console.WriteLine(mensajes[1] + e.OldStartingIndex);
+                    foreach (var item in e.OldItems)
+                    {
+                        Console.WriteLine(mensajes[2] + item.ToString());
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    Console.WriteLine(mensajes[1]);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+    }
+}
