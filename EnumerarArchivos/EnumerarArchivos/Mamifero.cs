837fbf1e1be35ff8be502bda009702d3b8970de8
Marcelo.Volta <mvolta@glasstone-mg.com>
2018-01-24 07:33:04 -0300

    Mejorar Ejemplo #14

---
+++ EnumerarArchivos/EnumerarArchivos/Mamifero.cs
@@ -0,0 +1,36 @@
+﻿namespace EnumerarArchivos
+{
+    class Mamifero
+    {
+        public int Peso { get; set; }
+        public int Edad { get; set; }
+
+        public string Especie { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            //Si obj no es un Mamifero lo siguiente da null
+            //Cubre también el caso en que obj sea null
+            var otroMamifero = obj as Mamifero;
+
+            return otroMamifero == null ?
+                false :
+                Equals(Peso, otroMamifero.Peso) &&
+                Equals(Edad, otroMamifero.Edad) &&
+                Equals(Especie.ToLower(), otroMamifero.Especie.ToLower());
+
+
+        }
+
+        public override int GetHashCode()
+        {
+            //Utilizando un método con primos (simplón)
+            int hash = 113;
+            hash = hash * (13 + Peso);
+            hash = hash * (13 + Edad);
+            hash = hash * (13 + Especie != null ? Especie.GetHashCode() : 0);
+
+            return hash;
+        }
+    }
+}
--- EnumerarArchivos/EnumerarArchivos/Program.cs
+++ EnumerarArchivos/EnumerarArchivos/Program.cs
@@ -520,7 +520,7 @@ namespace EnumerarArchivos
             #region Ejemplo #14 - Operadores de Conjuntos
             separador.EscribirEncabezado("Ejemplo #14 - Operadores de Conjuntos");
 
-            int[] multiplosDeDos = new int[] { 0, 2, 4, 6,8,10,12,14,16,18 };
+            int[] multiplosDeDos = new int[] { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18 };
             int[] multiplosDeTres = new int[] { 0, 3, 6, 9, 12, 15, 18, 21, 24, 27 };
 
             var multiplosDeDosYTres = multiplosDeDos.Intersect(multiplosDeTres);
@@ -529,34 +529,39 @@ namespace EnumerarArchivos
             {
                 Console.WriteLine(entero.ToString());
             }
+            Console.WriteLine();
 
-            separador.EscribirPie("Fin Ejemplo #14");
 
             var multiplosDeDosYMultiplosDeTres = multiplosDeDos.Union(multiplosDeTres);
-            Console.WriteLine("Union - Múltiplos de 2 y 3");
+            Console.WriteLine("Union - Múltiplos de 2 y Múltiplos de 3");
             foreach (var entero in multiplosDeDosYMultiplosDeTres)
             {
                 Console.WriteLine(entero.ToString());
             }
+            Console.WriteLine();
 
             var multiplosDeDosPeroNoDeTres = multiplosDeDos.Except(multiplosDeTres);
-            Console.WriteLine("Except - Multiplos de 2 que no son mùltiplos de 3");
+            Console.WriteLine("Except - Múltiplos de 2 que no son múltiplos de 3");
 
             foreach (var entero in multiplosDeDosPeroNoDeTres)
             {
                 Console.WriteLine(entero.ToString());
             }
+            Console.WriteLine();
 
             var multiplosDeTresPeroNoDeDos = multiplosDeTres.Except(multiplosDeDos);
-            Console.WriteLine("Except - Multiplos de 3 que no son mùltiplos de 2");
+            Console.WriteLine("Except - Múltiplos de 3 que no son múltiplos de 2");
 
             foreach (var entero in multiplosDeTresPeroNoDeDos)
             {
                 Console.WriteLine(entero.ToString());
             }
+            Console.WriteLine();
 
-
+            separador.EscribirPie("Fin Ejemplo #14");
             #endregion
+
+
 
             return;
 
